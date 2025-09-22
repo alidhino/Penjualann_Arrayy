bd885253b038952c864f65001958073e7c43ddcf
alidhino <cmr346330@gmail.com>
2025-09-23 01:20:00 +0700

    logika pembelian

--- index1.php
+++ index1.php
@@ -5,3 +5,17 @@ $harga_barang = [5000, 7000, 8000, 10000, 4000, 12000, 3500, 15000];
 
 echo "<h3>--POLGAN MART--</h3>";
 echo "Daftar Pembelian<br><br>";
+
+// Commit 2 – Logika pembelian
+$beli = [];
+$jumlah = [];
+$total = [];
+$grandtotal = 0;
+
+// misalnya kita pilih 5 barang random
+for ($i = 0; $i < 5; $i++) {
+    $index = array_rand($nama_barang);
+    $beli[] = $nama_barang[$index];
+    $jumlah[] = rand(1, 5);
+    $total[] = 0;
+}
